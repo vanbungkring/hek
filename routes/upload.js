85ac7b926e8bb95fc7da5cef1cc52927d8ffa5da
vanbungkring <prasetyo.mailbox@gmail.com>
2017-04-29 09:38:08 +0700

    update

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+var paths = require('path');
+var fs = require('fs');
 var request = require('request');
 const batch = parseInt(new Date().getTime() / 1000);
 const csv = require('csvtojson')
@@ -43,6 +45,33 @@ router.post('/', function(req, res, next) {
                 }
             })
 
+        })
+    });
+
+})
+router.post('/forest/', function(req, res, next) {
+    console.log(req.body)
+    if (!req.files) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    let file = req.files.csvFile;
+
+    file.mv('./public/source/csv_' + batch + '.csv', function(err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        var jsonData = [];
+        var csvData = [];
+        var path = ("./public/source/csv_" + batch + '.csv');
+        var pathString = paths.resolve(__dirname, '../public/source/','csv_'+batch + '.csv');
+        var url = 'http://nucare.zispro.co:5006/fort/log';
+        request.post({
+            url: url,
+            json: {'data':pathString,'target':'learning_label'}
+        }, function(err, httpResponse, body) {
+            if (err) {} else {
+              res.json(body);
+            }
         })
     });
 
